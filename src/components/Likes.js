63cce5a30858598aefdc215ec8e45ac24fabd63f
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2022-01-14 13:51:00 -0500

    debugging

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,31 +1,31 @@
 
 
-class Like extends React.Component() {
+// class Like extends React.Component() {
     
     
-    constructor(){
-        super()
-        this.state = {
-          liked: false
-        };
-      }
+//     constructor(){
+//         super()
+//         this.state = {
+//           liked: false
+//         };
+//       }
     
        
-   handleClick = (event) => {
-      this.setState ({
-          liked: !this.state.liked
-      })
-    }
+//    handleClick = (event) => {
+//       this.setState ({
+//           liked: !this.state.liked
+//       })
+//     }
 
-    render(){
-        const status = this.state.liked ? '👍' : '👎'
-        return(
-        <div>
-          <button onClick={this.handleClick}>{status}</button>
-        </div>
-      )
+//     render(){
+//         const status = this.state.liked ? '👍' : '👎'
+//         return(
+//         <div>
+//           <button onClick={this.handleClick}>{status}</button>
+//         </div>
+//       )
       
-    }
-}
+//     }
+// }
 
-export default Like
+// export default Like
